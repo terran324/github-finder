a5f8a8ac37b1a34a9b085c07ed9905f53647f9ab
Nicholas Ho <terran324@gmail.com>
2019-06-06 07:05:47 +1000

    Add navbar, fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Nicholas';
-    const loading = false;
-    const showName = true;
-    
     return (
       <div className="App">
-        {loading ? <h1>Loading...</h1> : <h1> Hello {showName && name}</h1>}
+        <Navbar />
+        <h1>hello</h1>
       </div>
     );
   }
