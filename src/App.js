f3887d9c21d8b5d3d8acc686b12194b586178f92
Nicholas Ho <terran324@gmail.com>
2019-06-16 22:25:05 +1000

    Added default props and proptypes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 
-class App extends Component {
+class App extends Component {  
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <h1>hello</h1>
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github'/>
       </div>
     );
   }
