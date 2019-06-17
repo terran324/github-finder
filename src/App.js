1f7f34fd47465fa5af96f28238b5c35d481fb278
Nicholas Ho <terran324@gmail.com>
2019-06-18 07:00:54 +1000

    Added UserItem to page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+
 import './App.css';
 
 class App extends Component {  
@@ -7,6 +9,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github'/>
+        <UserItem/>
       </div>
     );
   }
