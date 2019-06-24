4abe40dd517d7f8ea1e885568ad3caffda7ab902
Nicholas Ho <terran324@gmail.com>
2019-06-25 07:15:27 +1000

    Convert non-stateful components from class to functional

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+// ALTERNATIVELY:
+// const UserItem = (props) => {
+// const { avatar_url, login, html_url } = props.user;
 
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className="btn btn-dark btm sm my-1">
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className="btn btn-dark btm sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
 }
 
 export default UserItem;
