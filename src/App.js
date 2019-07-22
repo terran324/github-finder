751453142aa44f924112e98f75c310efa8b4ca7d
Nicholas Ho <terran324@gmail.com>
2019-07-23 07:26:27 +1000

    Added conditional to clear users from state if search not yet performed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
-import Axios from 'axios';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-class App extends Component {  
+class App extends Component {
   state = {
     users: [],
     loading: false
-  }
-  
+  };
+
+  /* Commented this away because we don't want users to show up when we havent searched for anything */
   // async componentDidMount() {
   //   this.setState({ loading: true });
-    
+
   //   const res = await Axios.get(`https://api.github.com/users?client_id=
   //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
   //     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
@@ -23,24 +24,33 @@ class App extends Component {
   // }
 
   // Search Github users
-  searchUsers = async (text) => {
+  searchUsers = async text => {
     this.setState({ loading: true });
     console.log(`Searched for: ${text}`);
-    
+
     const res = await Axios.get(`https://api.github.com/search/users?q=${text}&client_id=
     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ users: res.data.items, loading: false })
+    this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
+    const { users, loading } = this.state;
+
     return (
-      <div className='App'>
-        <Navbar title='Github Finder' icon='fab fa-github'/>
-        <div className='container'>
-          <Search searchUsers={this.searchUsers}/>
-          <Users loading={this.state.loading} users={this.state.users} />
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,40 +1,54 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
-    search: '' // The name of this key has to be equal to the name of the component that will be affected.
-               // In this case, it is the form, with the name of 'search'
+    search: "" // The name of this key has to be equal to the name of the component that will be affected.
+    // In this case, it is the form, with the name of 'search'
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
-  onSubmit = (e) => { // The `this` keyword works differently with arrow functions!
+  onSubmit = e => {
+    // The `this` keyword works differently with arrow functions!
     e.preventDefault();
     this.props.searchUsers(this.state.search);
-    this.setState({text:''});
+    this.setState({ text: "" });
   };
 
-  onChange = (e) => this.setState({[e.target.name]: e.target.value});
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
-          <input 
-            type="text" 
-            name="search" 
-            placeholder="Search Users..." 
+          <input
+            type="text"
+            name="search"
+            placeholder="Search Users..."
             value={this.state.search}
             onChange={this.onChange}
           />
-          <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Search
+export default Search;
